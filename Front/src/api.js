57a1e9ceab09d8a418b1c327aebd60ea3e4cce21
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-11-30 17:04:36 -0300

    melhorias 02

--- Front/src/api.js
+++ Front/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const url="http://localhost:3009/";
+export const url="http://109.123.243.212:3009/";
 export const api = axios.create({
     baseURL: url
     // http://109.123.243.212:3001/
