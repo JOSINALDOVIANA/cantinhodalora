edc85b66a582cce806c35a8a40fe55053b1263f8
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-12-07 14:24:51 -0300

    atualização knexfile

--- back/knexfile.js
+++ back/knexfile.js
@@ -1,23 +1,20 @@
-// Update with your config settings.
 
 export const development = {
-  client: 'mysql',
-  connection: {
-   
-    host: '109.123.243.212',
-    
-    user: 'cantinhodalora',
-    password: 'Wikazako@123*',
-
-    database: 'cantinhodalora',
-  },
-  migrations: {
-    directory: './src/databases/migrations'
-  },
-  seeds: {
-    directory: './src/databases/seeds'
-  }
-
-};
-
-
+    client: 'mysql',
+    connection: {
+     
+      host: '109.123.243.212',
+      
+      user: 'cantinhodalora',
+      password: 'Wikazako@123*',
+  
+      database: 'cantinhodalora',
+    },
+    migrations: {
+      directory: './src/databases/migrations'
+    },
+    seeds: {
+      directory: './src/databases/seeds'
+    }
+  
+  };
