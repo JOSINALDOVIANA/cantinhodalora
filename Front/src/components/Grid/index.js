155fa8fc89865aabcc3f2db5dc9133bd6edd185c
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-12-29 18:50:35 -0300

    correçoes

--- Front/src/components/Grid/index.js
+++ Front/src/components/Grid/index.js
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import ComplexGrid from './card';
 
 import { api, url } from '../../api';
+import { Paper, Typography } from '@mui/material';
 
 
 
@@ -13,6 +14,8 @@ import { api, url } from '../../api';
 export default function GridContainer() {
 
   const [produtos, setProd] = React.useState([]);
+  const [produtosFilter, setProdFilter] = React.useState([]);
+  const [categorias, setCat] = React.useState([]);
 
   React.useEffect(() => {
     api.get("/produtos").then(r => {
@@ -27,10 +30,34 @@ export default function GridContainer() {
           }
         }
         setProd(p)
+        setProdFilter(p)
       }
     })
   }, [])
-  // console.log(produtos)
+  React.useEffect(() => {
+    api.get("/categorias").then(r => {
+      
+      setCat(r.data.categorias)
+    })
+  }, [])
+  function filtro(id){
+    api.get(`/categorias?id=${id}`).then(r => {
+      let p = r.data.produtos
+      
+
+        for (const key in p) {
+          p[key].img.url = url + "images/" + p[key].img.key;
+          p[key].url = p[key].img.url;
+          for (const key2 in p[key].logos) {
+            p[key].logos[key2].url = url + "images/" + p[key].logos[key2].key
+          }
+        }
+      
+      
+      setProdFilter(p)
+    })
+  }
+  console.log(produtosFilter)
   return (
     <Box sx={{
       flexGrow: 1,
@@ -38,10 +65,20 @@ export default function GridContainer() {
       color: "#000",
       
     }}>
+      <Box className='carousel' sx={{display:"flex",flexGrow:1,margin:2,height:100,overflow:"scroll"}}>
+        <Paper onClick={()=>{setProdFilter(produtos)}} sx={{cursor:"pointer",display:"flex",justifyContent:"center",alignItems:"center",padding:2,margin:2}}><Typography>Todos</Typography></Paper>
+        {categorias.map(cat=>(
+          <Paper  key={cat.id} onClick={()=>{filtro(cat.id)}} sx={{cursor:"pointer",display:"flex",justifyContent:"center",alignItems:"center",padding:2,margin:2}}><Typography>{cat.desc}</Typography></Paper>
+        ))}
+        
+        
+       
+
+      </Box>
       <Grid container alignItems="center" spacing={2}>
 
 
-        {produtos?.map(p => (
+        {produtosFilter?.map(p => (
           <Grid key={p.id} item xs={6} sm={4} md={3} lg={3}>
             <ComplexGrid
               img={p.url}
