8c16d1c4f3ce06d325d91242f4eeb0ae8383e7d0
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2023-02-03 09:26:00 -0300

    atualização

--- Front/src/components/adm/relatorios/index.js
+++ Front/src/components/adm/relatorios/index.js
@@ -11,6 +11,7 @@ function Relatorios() {
   const [controleGer, setContGer] = React.useState('');
   const [closes, setCloses] = React.useState([]);
   const [closesFilter, setClosesF] = React.useState([]);
+  const [DateSelect, setDateS] = React.useState("2023-01-01");
 
 
   React.useEffect(() => {
@@ -43,14 +44,15 @@ function Relatorios() {
 
   const handleChangeConCol = (event) => {
     setContCOL(event.target.value);
-    setClosesF(closes.filter(c => c.id_col == event.target.value))
+    setClosesF(a=>{
+      return(closes.filter(c => (c.id_col == event.target.value) && (c.id_users==controleGer)))
+    })
   };
 
   const handleChangeConGer = (event) => {
-    setContGer(event.target.value);
-    // setFechamento(a => {
-    //     return ({ ...a, gerente: gerentes.filter(ger => ger.id == event.target.value)[0].id })
-    // })
+    setContGer(event.target.value)
+    setClosesF(closes.filter(g=>g.id_users==event.target.value));
+    
   };
 
   console.log(closesFilter)
@@ -67,6 +69,24 @@ function Relatorios() {
         className=''
       >
         <div style={{ width: "100%" }} className="row">
+        <div className="col-4">
+            <FormControl sx={{ width: "100%", margin: 1, background: "#fff" }}>
+              <InputLabel id="demo-simple-select-label">Gerente</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={controleGer}
+                label="Gerente"
+                onChange={handleChangeConGer}
+              >
+
+                {
+                  gerentes?.map((ger) => (<MenuItem key={ger.id + "ger"} value={ger.id}>{ger.name}</MenuItem>))
+                }
+
+              </Select>
+            </FormControl>
+          </div>
           <div className="col-4">
             <FormControl sx={{ width: "100%", margin: 1, background: "#fff" }}>
               <InputLabel id="demo-simple-select-label">Colaborador</InputLabel>
@@ -85,30 +105,23 @@ function Relatorios() {
               </Select>
             </FormControl>
           </div>
-          <div className="col-4">
-            <FormControl sx={{ width: "100%", margin: 1, background: "#fff" }}>
-              <InputLabel id="demo-simple-select-label">Gerente</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={controleGer}
-                label="Gerente"
-                onChange={handleChangeConGer}
-              >
-
-                {
-                  gerentes?.map((ger) => (<MenuItem key={ger.id + "ger"} value={ger.id}>{ger.name}</MenuItem>))
-                }
-
-              </Select>
-            </FormControl>
-          </div>
+         
           <div className="col-3 align-items-center justify-content-center " style={{ display: "flex" }}>
 
 
 
 
-            <input type="date" className='form-control' name="festa" min="2023-01-01" required />
+            <input type="date"
+            // value={DateSelect}
+            onSelect={e=>{
+              console.log(e)
+              // setDateS(e.target.value)
+              // setClosesF(a=>{
+              //   return(a.filter(e=>new Date(e.updated_at)==new Date(e.target.value)
+              //     ))
+              // })
+            }}
+             className='form-control' name="festa" min="2023-01-01"/>
 
 
 
@@ -118,16 +131,16 @@ function Relatorios() {
           </div>
         </div>
       </Paper>
-      <div  style={{ background: "#fff"}} className='row'>
-        <p className='col' style={{ width: "100%", textAlign: "center" }}>Colaborador</p>
-        <p className='col' style={{ width: "100%", textAlign: "center" }}>Gerente</p>
+      <div  style={{ background: "#ddd", display:"flex",height:"2em"}} className='row'>
+        <div className='col' style={{ width: "100%",height:"2em", textAlign: "center" , border:" solid  1px   #000" }}><p>Colaborador</p></div>
+        <div className='col ' style={{ width: "100%",height:"2em", textAlign: "center",border:" solid  1px   #000" }}><p>Gerente</p></div>
       </div>
-      {/* {closesFilter.map(item => (
-        <div key={item.id} style={{ background: "#fff", border: "solid 1px #dddd", display: "flex", alignItems: "center", justifyContent: "center" }} className='row'>
-          <p className='col' style={{ width: "100%", textAlign: "center" }}>{item.colaborador.name}</p>
-          <p className='col' style={{ width: "100%", textAlign: "center" }}>{item.gerente.name}</p>
+      {closesFilter.map(item => (
+        <div key={item.id} style={{ background: "#fff", display: "flex", alignItems: "center", justifyContent: "center" }} className='row'>
+          <p className='col m-3' style={{ width: "100%", textAlign: "initial" }}>{item.colaborador.name}</p>
+          <p className='col m-3' style={{ width: "100%", textAlign: "initial" }}>{item.gerente.name}</p>
         </div>
-      ))} */}
+      ))}
     </Box>
 
 
