3becb8165e0110a7f38ecb6b6019d17bd06adc3f
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2023-03-05 18:56:15 -0300

    correções interface

--- Front/src/components/Grid/card.js
+++ Front/src/components/Grid/card.js
@@ -5,8 +5,8 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { Badge, Box, Chip, useTheme } from '@mui/material';
 import { red, green } from '@mui/material/colors';
-import { border } from '@mui/system';
 
+import {FaceIcon,ListAlt} from '@mui/icons-material';
 
 
 const Img = styled('img')({
@@ -23,90 +23,87 @@ export default function ComplexGrid({ img, desc, tamanho, valor, logos, bg, id,u
     
     return (
 
-        <Paper
+    
+     <Paper
 
-            elevation={0}
-            sx={{
-                display: "flex",
-                borderRadius: 1,
-                flexDirection: "column",
-                fontFamily: "Roboto",
-                alignItems: "center",
-                position: "relative",
-                border: "solid 0.000001px #ddd",
-                // background:"#ddd"
-                
+        elevation={0}
+        sx={{
+            display: "flex",
+            borderRadius: 1,
+            flexDirection: "column",
+            fontFamily: "Roboto",
+            alignItems: "center",
+            background:und>=1?"transparent":"#ddd",
+            position: "relative",
+            border: "solid 0.000001px #ddd",
+            // background:"#ddd"
+            
 
 
-            }}
-            className='card marcado'
-            onClick={() => {
-                if (logos.length > 0) {
+        }}
+        className='card marcado'
+        onClick={() => {
+            if (logos.length > 0) {
 
-                    if (window.getComputedStyle(document.getElementById(id), null).display == "none") {
+                if (window.getComputedStyle(document.getElementById(id), null).display == "none") {
 
-                        document.getElementById(id).style.display = "flex"
-                        return
-                    }
-                    if (window.getComputedStyle(document.getElementById(id), null).display == "flex") {
-
-                        document.getElementById(id).style.display = "none"
-                        return
-                    }
-
+                    document.getElementById(id).style.display = "flex"
+                    return
                 }
-            }}
+                if (window.getComputedStyle(document.getElementById(id), null).display == "flex") {
 
-        >
+                    document.getElementById(id).style.display = "none"
+                    return
+                }
 
+            }
+        }}
 
-
-        <Badge sx={{right:25,top:15,position:"absolute"}} badgeContent={und} 
-        color={"success"}
-         max={999} >
-                
-            </Badge>
-
-        <div style={{width:100,height:100,marginTop:10,marginBottom:2}}>
-            <Img alt={desc} src={img} sx={{ borderRadius: 0,maxWidth:90,maxHeight:90, width: "auto", height: "auto",overflowClipMargin:"content-box",overflow:"clip" }} />
-            </div>
-            <Divider sx={{ margin: 1, marginTop: 2, width: "90%" }}>
-                <Chip label="Informações" />
-            </Divider>
-            {/* {logos.length > 0 && <Divider color="#000" sx={{ width: "90%" }} ></Divider>} */}
-            {logos.length > 0 && (
-
-                <Box id={id} sx={{ display: "none", maxWidth: "130px", justifyContent: "space-around", margin: 1 }}>
-
-                    {logos.map(logo => (<Img key={logo.id} alt='imagem' src={logo.url} sx={{ height: "30%", width: "30%" }}></Img>))}
-                </Box>
-
-            )}
-
-            {/* <Divider color="#000" sx={{ width: "90%" }} ></Divider> */}
-
-
-            {logos.length > 0 ? <Typography noWrap sx={{ fontSize: "0.6em", fontFamily: "Roboto", "&:hover": { cursor: "pointer" } }} color={red[600]}>CLIQUE PARA OPÇÕES</Typography> : <div style={{height:"0.9em",width:"100%"}}></div>}
-
-
-            <Box sx={{ flexDirection: "column", width: "100%", padding: 2 }}>
-
-                {bg ? null : <Typography noWrap sx={{ fontSize: "1.5em", fontFamily: "Roboto", fontWeight: 300, textAlign: "initial", marginTop: 2, color: "#050A30" }} variant="subtitle1" component="div">
-                    {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(valor)}
-                </Typography>}
-
-                <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
-                    <Typography sx={{ color: "#404E5C", fontSize: "0.9em",fontFamily:"Roboto" }} noWrap variant="subtitle1" component="p">
-                        {desc} {tamanho}
-                    </Typography>
-                    <Typography sx={{ fontSize: "0.8em" }} color={green[600]} noWrap gutterBottom variant="subtitle1" component="div">
-                        OBS: Verifique a disponibilidade
-                    </Typography>
+    >
 
 
 
-                </Box>
+    {und>1?<Badge sx={{right:25,top:15,position:"absolute"}} badgeContent={und} 
+    color={"success"}
+     max={999} >
+            
+        </Badge>:null}
 
+    <div style={{width:100,height:100,marginTop:10,marginBottom:2}}>
+        <Img alt={desc} src={img} sx={{ borderRadius: 0,maxWidth:90,maxHeight:90, width: "auto", height: "auto",overflowClipMargin:"content-box",overflow:"clip" }} />
+        </div>
+        <Divider sx={{ margin: 1, marginTop: 2, width: "90%" }}>
+            <Chip label="Informações" icon={<ListAlt color={"success"}/>} color={"success"} variant="outlined"/>
+        </Divider>
+        {/* {logos.length > 0 && <Divider color="#000" sx={{ width: "90%" }} ></Divider>} */}
+        {logos.length > 0 && (
+
+            <Box id={id} sx={{ display: "none", maxWidth: "130px", justifyContent: "space-around", margin: 1 }}>
+
+                {logos.map(logo => (<Img key={logo.id} alt='imagem' src={logo.url} sx={{ height: "30%", width: "30%" }}></Img>))}
+            </Box>
+
+        )}
+
+        {/* <Divider color="#000" sx={{ width: "90%" }} ></Divider> */}
+
+
+        {logos.length > 0 ? <Typography noWrap sx={{ fontSize: "0.6em", fontFamily: "Roboto", "&:hover": { cursor: "pointer" } }} color={red[600]}>CLIQUE PARA OPÇÕES</Typography> : <div style={{height:"0.9em",width:"100%"}}></div>}
+
+
+        <Box sx={{ flexDirection: "column", width: "100%", padding: 2 }}>
+
+            {bg ? null : <Typography noWrap sx={{ fontSize: "1.5em", fontFamily: "Roboto", fontWeight: 300, textAlign: "initial", marginTop: 2, color: "#050A30" }} variant="subtitle1" component="div">
+                {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(valor)}
+            </Typography>}
+
+            <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+                <Typography sx={{ color: "#404E5C", fontSize: "0.9em",fontFamily:"Roboto" }} noWrap variant="subtitle1" component="p">
+                    {desc} {tamanho}
+                </Typography>
+                <Typography sx={{ fontSize: "0.8em" }} color={green[600]} noWrap gutterBottom variant="subtitle1" component="div">
+                Verifique a disponibilidade
+                </Typography>
 
 
 
@@ -115,6 +112,13 @@ export default function ComplexGrid({ img, desc, tamanho, valor, logos, bg, id,u
 
 
 
-        </Paper>
+        </Box>
+
+
+
+
+    </Paper>
+  
+     
     );
 }
