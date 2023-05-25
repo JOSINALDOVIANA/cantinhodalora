7006bbc3cb3851b201f9c4ad92e33d15df820f85
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2023-05-25 14:28:39 -0300

    atualização 25-05 0.2

--- back/src/controller/categorias/index.js
+++ back/src/controller/categorias/index.js
@@ -20,7 +20,7 @@ export default {
                     produtos[key].img=await conexao("images").where({id:produtos[key].id_image}).first();
                     if(!!produtos[key].img){
                         produtos[key].img.url=process.env.IP_SERVER+":3009/images/"+produtos[key].img.key;
-                        produtos[key].url =process.env.IP_SERVER+":3009/images/"+produtos[key].img.key                                           
+                        produtos[key].url ="http://"+process.env.IP_SERVER+":3009/images/"+produtos[key].img.key                                           
                     }
                     produtos[key].logos=await conexao("image_prod").where({"image_prod.id_prod":produtos[key].id}).join("images","image_prod.id_image","=","images.id").select("images.*");
                     if(!!produtos[key].logos){
