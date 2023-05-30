b0c80bdee4fdb35b3a323a14f6b8fa4ea18b9a62
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2023-05-30 19:01:04 -0300

    atualização 30-05 0.1

--- front02/src/components/promocoes/index.js
+++ front02/src/components/promocoes/index.js
@@ -14,7 +14,7 @@ const Img = styled("img")({
 	maxWidth: "100%",
 	maxHeight: "100%",
 });
-function Promo({ bg }) {
+function Promo() {
 	const theme = useTheme();
 	const [promo, setPromo] = useState([]);
 	useEffect(() => {
@@ -23,21 +23,25 @@ function Promo({ bg }) {
 		})
 	}, [])
 
+
+
 	function Item({ i }) {
 		return (
 			<Box
 				sx={{
 					width: "100%",
 					display: "flex",
-					height:"auto",
+					minHeight:400,
+					maxHeight:400,
 					justifyContent: "center",
 					alignItems: "center",
-					
+					border:0
+
 				}}
 			>
 
 
-				<Paper
+				{/* <Paper
 
 					elevation={1}
 					sx={{
@@ -119,7 +123,97 @@ function Promo({ bg }) {
 					</Box>
 
 
+				</Paper> */}
+
+				<Paper
+				elevation={0}
+					sx={{
+
+						width: "100%",
+						display: "flex",						
+						justifyContent:"space-between",
+						alignItems:"center",
+						maxHeight:400,
+
+
+						[theme.breakpoints.down("sm")]: {
+							
+							
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "center"
+
+
+						}
+					}}
+				>
+					<img
+						alt={"test"}
+						src={i.id_prod ? i.prod?.img?.url : i?.img?.url}
+						height={300}
+						width={300}
+						
+					/>
+
+					<Typography sx={{ fontFamily: "Roboto", textAlign: "center" }} component="span">
+						{i.newdesc}
+					</Typography>
+
+					<Box
+						sx={{ 
+							flexDirection: "column", 
+							width: "30%", 
+							padding: 2,
+							[theme.breakpoints.down("sm")]:{
+								width:"100%"
+							} 
+							}}>
+						{!!i.id_prod ?
+							<Box sx={{ display: "flex" }}>
+								<Typography noWrap sx={{ fontFamily: "Roboto", fontWeight: 300, textAlign: "initial", marginRight: theme.spacing(2) }} variant="subtitle1" component="span">
+									De
+								</Typography>
+
+								<Typography noWrap
+									sx={{
+										fontSize: "1em",
+										fontFamily: "Roboto",
+										fontWeight: 300,
+										textAlign: "initial",
+										// marginTop: 2,
+										// color: "#ddd70",
+										textDecoration: "line-through"
+									}} variant="subtitle1" component="div">
+									{new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL", minimumFractionDigits: 2 }).format(i.prod.preco)}
+								</Typography>
+							</Box> : null}
+						<Box sx={{ display: "flex", alignItems: "center" }}>
+							<Typography noWrap sx={{ fontSize: "1em", marginRight: theme.spacing(2), fontFamily: "Roboto", fontWeight: 300, textAlign: "initial" }} variant="subtitle1" component="div">
+								Por
+							</Typography>
+							<Typography noWrap sx={{ fontSize: "1em", fontFamily: "Roboto", fontWeight: 300, textAlign: "initial", color: "#e02141" }} variant="subtitle1" component="div">
+								{new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL", minimumFractionDigits: 2 }).format(i.valpromo)}
+							</Typography>
+						</Box>
+						<Typography sx={{ fontSize: "0.8em", color: "#e02141" }} noWrap gutterBottom variant="subtitle1" component="div">
+							OBS: Enquanto durar o estoque
+						</Typography>
+
+
+
+
+
+
+
+
+					</Box>
+
+
+
+
 				</Paper>
+
+
 			</Box>
 
 
@@ -132,7 +226,7 @@ function Promo({ bg }) {
 	return (
 
 
-	<Carousel sx={{height:"auto",width:"100%"}}   indicators>
+		<Carousel  indicators>
 
 
 
