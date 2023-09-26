ef4f7e46392f9f2ba2260e97091c0af2a094b517
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2023-09-26 19:24:19 -0300

    atualização

--- back/src/controller/images/index.js
+++ back/src/controller/images/index.js
@@ -62,10 +62,10 @@ export default {
         try {
             let images=await conexao("image_cli").where({prod:false,id_cli}).join("images","image_cli.id_image","=","images.id").select("images.*")
             console.log(images)
-            // for (const key in images) {
-            //    images[key].delete=`http://${process.env.IP_SERVER}:3009/deleteImage?id=${images[key].id}&key=${images[key].key}`
-            //    images[key].url=`http://${process.env.IP_SERVER}:3009/images/${images[key].key}`;
-            // }
+            for (const key in images) {
+               images[key].delete=`http://${process.env.IP_SERVER}:3009/deleteImage?id=${images[key].id}&key=${images[key].key}`
+               images[key].url=`http://${process.env.IP_SERVER}:3009/images/${images[key].key}`;
+            }
             return res.json({status:true,images})
         } catch (error) {
             console.log(error)
